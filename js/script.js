ce7dac72cccec19b8848dc6170aff922fbb8c0f9
golamsarwar98 <golamsarwar1896@gmail.com>
2024-09-23 17:33:35 +0600

    figured out how to implement the donation part. everything is working fine. Will try to make a common function for all the cards.

--- js/script.js
+++ js/script.js
@@ -15,3 +15,47 @@ mainHistoryBtn.addEventListener('click',function(){
 
     showForm('history-form');
 })
+
+//Donation Calculation
+document.getElementById('first-donation-btn')
+.addEventListener('click',function(){
+    
+    //Overall balance
+    const  mainBalanceNumber = getTextInput('main-balance');
+    console.log(mainBalanceNumber);
+
+    //Validation : main balance should be greater than 0
+    if(mainBalanceNumber<=0){
+        alert('You do not have sufficient balance');
+        return;
+    }
+
+    //capturing donation input.
+    const firstDonation = document.getElementById('first-donation-field').value;
+    //converting it to a floating number
+    const firstDonationNumber = parseFloat(firstDonation);
+
+    //Validation : check if the number is a string or not.
+    if(isNaN(firstDonationNumber)){
+        alert('Not a valid number');
+        return;
+    }
+
+    //Converting current donation value to number;
+    const currentDonationNumber = getTextInput('current-donation');
+    console.log(currentDonationNumber);
+
+    //Updated donation after a donation.
+    const updatedDonation = firstDonationNumber + currentDonationNumber;
+
+    //Setting up the balance in frontend
+    document.getElementById('current-donation').innerText = `${updatedDonation} BDT`;
+
+    //Overall balance after making a donation
+    const updatedMainBalance = mainBalanceNumber - firstDonationNumber;
+    console.log(updatedMainBalance);
+
+    //Setting up main balance in frontend.
+    document.getElementById('main-balance').innerHTML = `${updatedMainBalance} BDT`;
+
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,30 @@
-function showForm(id){
-    document.getElementById('donation-form').classList.add('hidden');
-    document.getElementById('history-form').classList.add('hidden');
+//Toggle donation and history page
+function showForm(id) {
+  document.getElementById("donation-form").classList.add("hidden");
+  document.getElementById("history-form").classList.add("hidden");
 
-    document.getElementById(id).classList.remove('hidden');
+  document.getElementById(id).classList.remove("hidden");
 }
+
+//Converting String to Number
+function stringToNumber(str) {
+  let numbers = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      numbers += str[i];
+    }
+  }
+
+  return parseFloat(numbers);
+}
+
+function getTextInput(id) {
+  //Getting the current donation amount
+  const currentDonation = document.getElementById(id).innerText;
+  //Converted the current donation to a floating number by making function
+  const currentDonationNumber = stringToNumber(currentDonation);
+
+  return currentDonationNumber;
+}
+
+function getDonation(id) {}
